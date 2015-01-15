eed8ad78cec9e2927cd1e6dc1a0494171d7c7a5a
Samuel Marguerat <smarguer@mrc-8384.cscdom.csc.mrc.ac.uk>
2015-01-15 17:05:12 +0000

    commit before re-install

---
+++ analysis_150115.sh
@@ -0,0 +1,303 @@
+###chr
+###controls
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I3.5mis.ALLchr.col > ALLchr_20_1_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I4.5mis.ALLchr.col > ALLchr_20_2_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I5.5mis.ALLchr.col > ALLchr_20_1_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I6.5mis.ALLchr.col > ALLchr_20_2_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I7.5mis.ALLchr.col > ALLchr_20_1_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I8.5mis.ALLchr.col > ALLchr_20_2_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I9.5mis.ALLchr.col > ALLchr_20_3_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I10.5mis.ALLchr.col > ALLchr_20_4_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I11.5mis.ALLchr.col > ALLchr_20_5_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I12.5mis.ALLchr.col > ALLchr_20_6_1
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I3.5mis.ALLchr.col > ALLchr_25_1_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I4.5mis.ALLchr.col > ALLchr_25_2_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I5.5mis.ALLchr.col > ALLchr_25_1_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I6.5mis.ALLchr.col > ALLchr_25_2_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I7.5mis.ALLchr.col > ALLchr_25_1_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I8.5mis.ALLchr.col > ALLchr_25_2_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I9.5mis.ALLchr.col > ALLchr_25_3_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I10.5mis.ALLchr.col > ALLchr_25_4_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I11.5mis.ALLchr.col > ALLchr_25_5_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I12.5mis.ALLchr.col > ALLchr_25_6_1
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I3.5mis.ALLchr.col > ALLchr_34_1_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I4.5mis.ALLchr.col > ALLchr_34_2_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I5.5mis.ALLchr.col > ALLchr_34_1_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I6.5mis.ALLchr.col > ALLchr_34_2_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I7.5mis.ALLchr.col > ALLchr_34_1_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I8.5mis.ALLchr.col > ALLchr_34_2_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I9.5mis.ALLchr.col > ALLchr_34_3_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I10.5mis.ALLchr.col > ALLchr_34_4_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I11.5mis.ALLchr.col > ALLchr_34_5_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I12.5mis.ALLchr.col > ALLchr_34_6_1
+###cells
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I1.5mis.ALLchr.col > ALLchr_20_sc1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I2.5mis.ALLchr.col > ALLchr_20_sc2
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I3.5mis.ALLchr.col > ALLchr_20_sc3
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I4.5mis.ALLchr.col > ALLchr_20_sc4
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I5.5mis.ALLchr.col > ALLchr_20_sc5
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I6.5mis.ALLchr.col > ALLchr_20_sc6
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I7.5mis.ALLchr.col > ALLchr_20_sc7
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I8.5mis.ALLchr.col > ALLchr_20_sc8
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I9.5mis.ALLchr.col > ALLchr_20_sc9
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I10.5mis.ALLchr.col > ALLchr_20_sc10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I11.5mis.ALLchr.col > ALLchr_20_sc11
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I12.5mis.ALLchr.col > ALLchr_20_sc12
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I1.5mis.ALLchr.col > ALLchr_25_sc1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I2.5mis.ALLchr.col > ALLchr_25_sc2
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I3.5mis.ALLchr.col > ALLchr_25_sc3
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I4.5mis.ALLchr.col > ALLchr_25_sc4
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I5.5mis.ALLchr.col > ALLchr_25_sc5
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I6.5mis.ALLchr.col > ALLchr_25_sc6
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I7.5mis.ALLchr.col > ALLchr_25_sc7
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I8.5mis.ALLchr.col > ALLchr_25_sc8
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I9.5mis.ALLchr.col > ALLchr_25_sc9
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I10.5mis.ALLchr.col > ALLchr_25_sc10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I11.5mis.ALLchr.col > ALLchr_25_sc11
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I12.5mis.ALLchr.col > ALLchr_25_sc12
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I1.5mis.ALLchr.col > ALLchr_34_sc1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I2.5mis.ALLchr.col > ALLchr_34_sc2
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I3.5mis.ALLchr.col > ALLchr_34_sc3
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I4.5mis.ALLchr.col > ALLchr_34_sc4
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I5.5mis.ALLchr.col > ALLchr_34_sc5
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I6.5mis.ALLchr.col > ALLchr_34_sc6
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I7.5mis.ALLchr.col > ALLchr_34_sc7
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I8.5mis.ALLchr.col > ALLchr_34_sc8
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I9.5mis.ALLchr.col > ALLchr_34_sc9
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I10.5mis.ALLchr.col > ALLchr_34_sc10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I11.5mis.ALLchr.col > ALLchr_34_sc11
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I12.5mis.ALLchr.col > ALLchr_34_sc12
+
+###trs
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I3.5mis.ALLtrs.GEN.col > ALLtrs_20_1_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I4.5mis.ALLtrs.GEN.col > ALLtrs_20_2_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I5.5mis.ALLtrs.GEN.col > ALLtrs_20_1_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I6.5mis.ALLtrs.GEN.col > ALLtrs_20_2_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I7.5mis.ALLtrs.GEN.col > ALLtrs_20_1_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I8.5mis.ALLtrs.GEN.col > ALLtrs_20_2_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I9.5mis.ALLtrs.GEN.col > ALLtrs_20_3_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I10.5mis.ALLtrs.GEN.col > ALLtrs_20_4_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I11.5mis.ALLtrs.GEN.col > ALLtrs_20_5_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_3_R2_I12.5mis.ALLtrs.GEN.col > ALLtrs_20_6_1
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I3.5mis.ALLtrs.GEN.col > ALLtrs_25_1_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I4.5mis.ALLtrs.GEN.col > ALLtrs_25_2_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I5.5mis.ALLtrs.GEN.col > ALLtrs_25_1_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I6.5mis.ALLtrs.GEN.col > ALLtrs_25_2_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I7.5mis.ALLtrs.GEN.col > ALLtrs_25_1_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I8.5mis.ALLtrs.GEN.col > ALLtrs_25_2_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I9.5mis.ALLtrs.GEN.col > ALLtrs_25_3_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I10.5mis.ALLtrs.GEN.col > ALLtrs_25_4_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I11.5mis.ALLtrs.GEN.col > ALLtrs_25_5_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_4_R2_I12.5mis.ALLtrs.GEN.col > ALLtrs_25_6_1
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I3.5mis.ALLtrs.GEN.col > ALLtrs_34_1_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I4.5mis.ALLtrs.GEN.col > ALLtrs_34_2_100
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I5.5mis.ALLtrs.GEN.col > ALLtrs_34_1_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I6.5mis.ALLtrs.GEN.col > ALLtrs_34_2_10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I7.5mis.ALLtrs.GEN.col > ALLtrs_34_1_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I8.5mis.ALLtrs.GEN.col > ALLtrs_34_2_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I9.5mis.ALLtrs.GEN.col > ALLtrs_34_3_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I10.5mis.ALLtrs.GEN.col > ALLtrs_34_4_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I11.5mis.ALLtrs.GEN.col > ALLtrs_34_5_1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_2_R2_I12.5mis.ALLtrs.GEN.col > ALLtrs_34_6_1
+###cells
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I1.5mis.ALLtrs.GEN.col > ALLtrs_20_sc1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I2.5mis.ALLtrs.GEN.col > ALLtrs_20_sc2
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I3.5mis.ALLtrs.GEN.col > ALLtrs_20_sc3
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I4.5mis.ALLtrs.GEN.col > ALLtrs_20_sc4
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I5.5mis.ALLtrs.GEN.col > ALLtrs_20_sc5
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I6.5mis.ALLtrs.GEN.col > ALLtrs_20_sc6
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I7.5mis.ALLtrs.GEN.col > ALLtrs_20_sc7
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I8.5mis.ALLtrs.GEN.col > ALLtrs_20_sc8
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I9.5mis.ALLtrs.GEN.col > ALLtrs_20_sc9
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I10.5mis.ALLtrs.GEN.col > ALLtrs_20_sc10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I11.5mis.ALLtrs.GEN.col > ALLtrs_20_sc11
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_1_R2_I12.5mis.ALLtrs.GEN.col > ALLtrs_20_sc12
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I1.5mis.ALLtrs.GEN.col > ALLtrs_25_sc1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I2.5mis.ALLtrs.GEN.col > ALLtrs_25_sc2
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I3.5mis.ALLtrs.GEN.col > ALLtrs_25_sc3
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I4.5mis.ALLtrs.GEN.col > ALLtrs_25_sc4
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I5.5mis.ALLtrs.GEN.col > ALLtrs_25_sc5
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I6.5mis.ALLtrs.GEN.col > ALLtrs_25_sc6
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I7.5mis.ALLtrs.GEN.col > ALLtrs_25_sc7
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I8.5mis.ALLtrs.GEN.col > ALLtrs_25_sc8
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I9.5mis.ALLtrs.GEN.col > ALLtrs_25_sc9
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I10.5mis.ALLtrs.GEN.col > ALLtrs_25_sc10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I11.5mis.ALLtrs.GEN.col > ALLtrs_25_sc11
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 1212_2_R2_I12.5mis.ALLtrs.GEN.col > ALLtrs_25_sc12
+
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I1.5mis.ALLtrs.GEN.col > ALLtrs_34_sc1
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I2.5mis.ALLtrs.GEN.col > ALLtrs_34_sc2
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I3.5mis.ALLtrs.GEN.col > ALLtrs_34_sc3
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I4.5mis.ALLtrs.GEN.col > ALLtrs_34_sc4
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I5.5mis.ALLtrs.GEN.col > ALLtrs_34_sc5
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I6.5mis.ALLtrs.GEN.col > ALLtrs_34_sc6
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I7.5mis.ALLtrs.GEN.col > ALLtrs_34_sc7
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I8.5mis.ALLtrs.GEN.col > ALLtrs_34_sc8
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I9.5mis.ALLtrs.GEN.col > ALLtrs_34_sc9
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I10.5mis.ALLtrs.GEN.col > ALLtrs_34_sc10
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I11.5mis.ALLtrs.GEN.col > ALLtrs_34_sc11
+perl ~/DATA/SC_2014/SCRB_SCRIPT/UMIfilter.pl 0512_1_R2_I12.5mis.ALLtrs.GEN.col > ALLtrs_34_sc12
+
+rename R2 R3 filtered*
+rename filtered. "" filtered*
+
+
+
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I2.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I3.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I4.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I5.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I6.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I7.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I8.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I9.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I10.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I11.sh
+cp sam_job_R3_1212_1_I1.sh sam_job_R3_1212_1_I12.sh
+
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I2\"/' sam_job_R3_1212_1_I2.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I3\"/' sam_job_R3_1212_1_I3.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I4\"/' sam_job_R3_1212_1_I4.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I5\"/' sam_job_R3_1212_1_I5.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I6\"/' sam_job_R3_1212_1_I6.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I7\"/' sam_job_R3_1212_1_I7.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I8\"/' sam_job_R3_1212_1_I8.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I9\"/' sam_job_R3_1212_1_I9.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I10\"/' sam_job_R3_1212_1_I10.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I11\"/' sam_job_R3_1212_1_I11.sh
+sed -i -e 's/suf\=\"_I1\"/suf\=\"_I12\"/' sam_job_R3_1212_1_I12.sh
+
+
+pax -rw -pp -k -s /1212_1/1212_2/ * .
+pax -rw -pp -k -s /1212_1/1212_3/ * .
+pax -rw -pp -k -s /1212_1/1212_4/ * .
+pax -rw -pp -k -s /1212_1/0512_1/ * .
+pax -rw -pp -k -s /1212_1/0512_2/ * .
+
+sed -i -e 's/toDo=\"1212_1_R3\"/toDo=\"1212_2_R3\"/' sam_job_R3_1212_2_I*
+sed -i -e 's/toDo=\"1212_1_R3\"/toDo=\"1212_3_R3\"/' sam_job_R3_1212_3_I*
+sed -i -e 's/toDo=\"1212_1_R3\"/toDo=\"1212_4_R3\"/' sam_job_R3_1212_4_I*
+sed -i -e 's/toDo=\"1212_1_R3\"/toDo=\"0512_1_R3\"/' sam_job_R3_0512_1_I*
+sed -i -e 's/toDo=\"1212_1_R3\"/toDo=\"0512_2_R3\"/' sam_job_R3_0512_2_I*
+
+
+qsub sam_job_R3_1212_1_I1.sh &
+
+
+
+qsub sam_job_R3_1212_1_I2.sh &
+qsub sam_job_R3_1212_1_I3.sh &
+qsub sam_job_R3_1212_1_I4.sh &
+qsub sam_job_R3_1212_1_I5.sh &
+qsub sam_job_R3_1212_1_I6.sh &
+qsub sam_job_R3_1212_1_I7.sh &
+qsub sam_job_R3_1212_1_I8.sh &
+qsub sam_job_R3_1212_1_I9.sh &
+qsub sam_job_R3_1212_1_I10.sh &
+qsub sam_job_R3_1212_1_I11.sh &
+qsub sam_job_R3_1212_1_I12.sh &
+
+qsub sam_job_R3_1212_2_I1.sh &
+qsub sam_job_R3_1212_2_I2.sh &
+qsub sam_job_R3_1212_2_I3.sh &
+qsub sam_job_R3_1212_2_I4.sh &
+qsub sam_job_R3_1212_2_I5.sh &
+qsub sam_job_R3_1212_2_I6.sh &
+qsub sam_job_R3_1212_2_I7.sh &
+qsub sam_job_R3_1212_2_I8.sh &
+qsub sam_job_R3_1212_2_I9.sh &
+qsub sam_job_R3_1212_2_I10.sh &
+qsub sam_job_R3_1212_2_I11.sh &
+qsub sam_job_R3_1212_2_I12.sh &
+
+qsub sam_job_R3_1212_3_I1.sh &
+qsub sam_job_R3_1212_3_I2.sh &
+qsub sam_job_R3_1212_3_I3.sh &
+qsub sam_job_R3_1212_3_I4.sh &
+qsub sam_job_R3_1212_3_I5.sh &
+qsub sam_job_R3_1212_3_I6.sh &
+qsub sam_job_R3_1212_3_I7.sh &
+qsub sam_job_R3_1212_3_I8.sh &
+qsub sam_job_R3_1212_3_I9.sh &
+qsub sam_job_R3_1212_3_I10.sh &
+qsub sam_job_R3_1212_3_I11.sh &
+qsub sam_job_R3_1212_3_I12.sh &
+
+qsub sam_job_R3_1212_4_I1.sh &
+qsub sam_job_R3_1212_4_I2.sh &
+qsub sam_job_R3_1212_4_I3.sh &
+qsub sam_job_R3_1212_4_I4.sh &
+qsub sam_job_R3_1212_4_I5.sh &
+qsub sam_job_R3_1212_4_I6.sh &
+qsub sam_job_R3_1212_4_I7.sh &
+qsub sam_job_R3_1212_4_I8.sh &
+qsub sam_job_R3_1212_4_I9.sh &
+qsub sam_job_R3_1212_4_I10.sh &
+qsub sam_job_R3_1212_4_I11.sh &
+qsub sam_job_R3_1212_4_I12.sh &
+
+qsub sam_job_R3_0512_1_I1.sh &
+qsub sam_job_R3_0512_1_I2.sh &
+qsub sam_job_R3_0512_1_I3.sh &
+qsub sam_job_R3_0512_1_I4.sh &
+qsub sam_job_R3_0512_1_I5.sh &
+qsub sam_job_R3_0512_1_I6.sh &
+qsub sam_job_R3_0512_1_I7.sh &
+qsub sam_job_R3_0512_1_I8.sh &
+qsub sam_job_R3_0512_1_I9.sh &
+qsub sam_job_R3_0512_1_I10.sh &
+qsub sam_job_R3_0512_1_I11.sh &
+qsub sam_job_R3_0512_1_I12.sh &
+
+qsub sam_job_R3_0512_2_I1.sh &
+qsub sam_job_R3_0512_2_I2.sh &
+qsub sam_job_R3_0512_2_I3.sh &
+qsub sam_job_R3_0512_2_I4.sh &
+qsub sam_job_R3_0512_2_I5.sh &
+qsub sam_job_R3_0512_2_I6.sh &
+qsub sam_job_R3_0512_2_I7.sh &
+qsub sam_job_R3_0512_2_I8.sh &
+qsub sam_job_R3_0512_2_I9.sh &
+qsub sam_job_R3_0512_2_I10.sh &
+qsub sam_job_R3_0512_2_I11.sh &
+qsub sam_job_R3_0512_2_I12.sh &
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
